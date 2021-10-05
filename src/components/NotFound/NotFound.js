d4868819e7b21419e1dfc63dce9e056832317119
Joyanta paul <joyantapauljoy11@gmail.com>
2021-10-05 14:50:08 +0600

    add 3

--- src/components/NotFound/NotFound.js
+++ src/components/NotFound/NotFound.js
@@ -2,8 +2,9 @@ import React from 'react';
 
 const NotFound = () => {
     return (
-        <div>
-            
+        <div className="bg-info">
+            <h1 className=" text-primary text-center ">404</h1>
+            <h2 className="text-danger text-center"> This Page Is not WOrking</h2>
         </div>
     );
 };
